46b7698946275f2b12b2f35122f60a8f56f1e371
Astrid Yu <astrid@astrid.tech>
2023-12-24 20:39:16 -0800

    Refactor compact-poker to be safer and nicer

--- compact-poker/src/lib.rs
+++ compact-poker/src/lib.rs
@@ -28,56 +28,127 @@
 //! transformed into this format. The sort key is always the shrunken representation
 //! of the cards.
 
-use poker::Card;
+use itertools::Itertools;
+use poker::{Card, Rank, Suit};
+use variter::VarIter;
 
-pub fn shrink_card(card: Card) -> u8 {
-    let suit = card.suit() as u8;
-    let rank = (card.unique_integer() >> 8) as u8 & 0xf;
-    rank | (suit << 4)
+/// A reduced representation of a card.
+#[derive(Debug, PartialOrd, Ord, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct SCard(u8);
+
+impl SCard {
+    #[inline]
+    pub fn new(r: Rank, s: Suit) -> Self {
+        Self((s as u8) << 4 | r as u8)
+    }
+
+    #[inline]
+    unsafe fn unsafe_from_raw(raw: u8) -> Self {
+        SCard(raw)
+    }
+
+    #[inline]
+    pub fn raw(&self) -> u8 {
+        self.0
+    }
+
+    pub fn deck() -> impl Iterator<Item = SCard> {
+        Suit::ALL_VARIANTS
+            .iter()
+            .cartesian_product(Rank::ALL_VARIANTS.iter())
+            .map(|(s, r)| SCard::new(*r, *s))
+    }
+
+    #[inline]
+    pub fn suit(&self) -> Suit {
+        let suit = (self.0 & 0x30) >> 4;
+        unsafe { std::mem::transmute(suit) }
+    }
+
+    #[inline]
+    pub fn rank(&self) -> Rank {
+        let rank = self.0 & 0xf;
+        unsafe { std::mem::transmute(rank) }
+    }
 }
 
-pub unsafe fn unsafe_grow_card(card: u8) -> Card {
-    let suit = (card & 0x30) >> 4;
-    let rank = card & 0xf;
-    Card::new(std::mem::transmute(rank), std::mem::transmute(suit))
+impl From<Card> for SCard {
+    fn from(card: Card) -> Self {
+        let suit = card.suit() as u8;
+        let rank = (card.unique_integer() >> 8) as u8 & 0xf;
+        SCard(rank | (suit << 4))
+    }
 }
 
-pub fn shrink_hand(hand: &[Card]) -> u32 {
-    let mut shrunk = [
-        shrink_card(hand[0]),
-        shrink_card(hand[1]),
-        shrink_card(hand[2]),
-        shrink_card(hand[3]),
-        shrink_card(hand[4]),
-    ];
-    shrunk.sort();
-
-    ((shrunk[4] as u32) << 24)
-        | ((shrunk[3] as u32) << 18)
-        | ((shrunk[2] as u32) << 12)
-        | ((shrunk[1] as u32) << 6)
-        | shrunk[0] as u32
+impl From<SCard> for Card {
+    fn from(c: SCard) -> Self {
+        Card::new(c.rank(), c.suit())
+    }
 }
 
-#[allow(overflowing_literals)]
-pub unsafe fn unsafe_grow_hand(hand: u32) -> [Card; 5] {
-    [
-        unsafe_grow_card(hand as u8),
-        unsafe_grow_card((hand >> 6) as u8),
-        unsafe_grow_card((hand >> 12) as u8),
-        unsafe_grow_card((hand >> 18) as u8),
-        unsafe_grow_card((hand >> 24) as u8),
-    ]
+/// A reduced representation of a hand.
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct SHand(u32);
+
+impl SHand {
+    pub unsafe fn unsafe_from_raw(hand: u32) -> Self {
+        SHand(hand)
+    }
+
+    #[allow(overflowing_literals)]
+    pub fn members(hand: u32) -> [SCard; 5] {
+        unsafe {
+            [
+                SCard::unsafe_from_raw(hand as u8),
+                SCard::unsafe_from_raw((hand >> 6) as u8),
+                SCard::unsafe_from_raw((hand >> 12) as u8),
+                SCard::unsafe_from_raw((hand >> 18) as u8),
+                SCard::unsafe_from_raw((hand >> 24) as u8),
+            ]
+        }
+    }
+}
+
+impl From<&[SCard]> for SHand {
+    fn from(hand: &[SCard]) -> Self {
+        let mut shrunk = [hand[0].0, hand[1].0, hand[2].0, hand[3].0, hand[4].0];
+        shrunk.sort();
+
+        SHand(
+            ((shrunk[4] as u32) << 24)
+                | ((shrunk[3] as u32) << 18)
+                | ((shrunk[2] as u32) << 12)
+                | ((shrunk[1] as u32) << 6)
+                | shrunk[0] as u32,
+        )
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
+
     use super::*;
 
     #[test]
-    fn roundtrip_cards() {
-        for c in Card::generate_deck() {
-            unsafe { assert_eq!(unsafe_grow_card(shrink_card(c)), c) }
+    fn roundtrip_large_cards_to_scards() {
+        for lc in Card::generate_deck() {
+            assert_eq!(Card::from(SCard::from(lc)), lc)
         }
+    }
+
+    #[test]
+    fn scards_have_right_data() {
+        for lc in Card::generate_deck() {
+            let sc = SCard::from(lc);
+            assert_eq!((sc.rank(), sc.suit()), (lc.rank(), lc.suit()));
+        }
+    }
+
+    #[test]
+    fn scards_generate_correctly() {
+        let lcs = Card::generate_deck().map(SCard::from).collect::<HashSet<_>>();
+        let scs = SCard::deck().collect::<HashSet<_>>();
+        assert_eq!(lcs, scs);
     }
 }
